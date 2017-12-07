552eb4a1bc7c88a7791a58a8f2242aeec16235da
Sergey Zhuga <szhuga@cloudaware.com>
2017-12-07 14:49:37 +0200

    Fix breeze_agent module according to the puppet-lint recomendations

--- puppet-modules/breeze_agent/manifests/init.pp
+++ puppet-modules/breeze_agent/manifests/init.pp
@@ -1,42 +1,45 @@
+# Install and configure the breeze_agent module
 class breeze_agent (
   $breeze_package_linux   = $::breeze_package_linux,
   $breeze_package_windows = $::breeze_package_windows,
 ) {
 
-  if $kernel == 'Linux' {
+  if $::kernel == 'Linux' {
 
     file { 'breeze-agent':
-      path   => "/tmp/breeze-agent.linux.tgz",
       ensure => present,
+      path   => '/tmp/breeze-agent.linux.tgz',
       source => "puppet:///modules/breeze_agent/${breeze_package_linux}",
     }
 
     exec { 'unpack-breeze':
       path    => ['/usr/bin', '/usr/sbin', '/bin', '/usr/local/sbin', '/sbin'],
-      command => "tar zxf /tmp/breeze-agent.linux.tgz -C /opt",
-      creates => "/opt/breeze-agent/app.rb",
+      command => 'tar zxf /tmp/breeze-agent.linux.tgz -C /opt',
+      creates => '/opt/breeze-agent/app.rb',
       require => File['breeze-agent'],
     }
 
     file { 'cron-file':
-      path    => "/etc/cron.d/breeze-agent",
       ensure  => present,
+      path    => '/etc/cron.d/breeze-agent',
       require => Exec['unpack-breeze'],
-      content => "*/15 * * * * root /opt/breeze-agent/app.sh >> /var/log/breeze-agent.log 2>&1\n",
+      content => "*/15 * * * * root /opt/breeze-agent/app.sh >> /var/log/breeze-agent.log 2>&1\n", # lint:ignore:80chars
+
     }
 
   } else {
 
-      file { 'breeze-agent':
-        path   => "C:\\breeze-agent.exe",
-        ensure => present,
-        source => "puppet:///modules/breeze_agent/${breeze_package_windows}",
-      }
-
-      exec { "install-breeze":
-        command => "C:\\breeze-agent.exe",
-        creates => "C:\\Program Files\\Breeze\\app.bat",
-      }
+    file { 'breeze-agent':
+      ensure => present,
+      path   => 'C:\\breeze-agent.exe',
+      source => "puppet:///modules/breeze_agent/${breeze_package_windows}",
     }
+
+    exec { 'install-breeze':
+      command => 'C:\\breeze-agent.exe',
+      creates => 'C:\\Program Files\\Breeze\\app.bat',
+    }
+
+  }
 
 }
